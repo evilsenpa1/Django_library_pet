e223c79fdc33d3520056a019f9a865f3efb7d8e7
evilsenpa1 <alexcorporationo@gmail.com>
2026-03-21 19:57:41 +0100

    Adding MtM field to serializer books

--- books/serializers.py
+++ books/serializers.py
@@ -1,7 +1,12 @@
 from rest_framework import serializers
-from books.models import BookModel
+from .models import BookModel
+from authors.models import AuthorModel
 
 class BookSerializer(serializers.ModelSerializer):
+    authors = serializers.PrimaryKeyRelatedField(
+        many=True,
+        queryset=AuthorModel.objects.all(),
+    )
 
     class Meta:
         model = BookModel
