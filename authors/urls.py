687778bded55535db3ef2ba2361e80a3761798c3
evilsenpa1 <alexcorporationo@gmail.com>
2026-04-25 21:14:48 +0200

    fix(urls): rename APP_NAME to app_name for correct namespace resolution
    Django URL namespacing requires the lowercase app_name variable;
    APP_NAME was silently ignored, breaking reverse() lookups.

--- authors/urls.py
+++ authors/urls.py
@@ -2,7 +2,7 @@ from rest_framework.routers import DefaultRouter
 
 from .views import AuthorViewSet
 
-APP_NAME = "authors"
+app_name = "authors"
 
 router = DefaultRouter()
 router.register(r"authors", AuthorViewSet, basename="authors")
