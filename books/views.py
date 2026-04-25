46f6e12a1a4ba3bf46c51af699e84c21eee991a1
evilsenpa1 <alexcorporationo@gmail.com>
2026-04-25 21:14:35 +0200

    refactor(views): use IsAuthenticatedOrReadOnly and prefetch_related
    - Replace AllowAny with IsAuthenticatedOrReadOnly in BookViewSet and AuthorViewSet
    - Add prefetch_related('books') and prefetch_related('authors') to avoid N+1 queries

--- books/views.py
+++ books/views.py
@@ -1,11 +1,11 @@
 
 from rest_framework import viewsets
-from rest_framework.permissions import AllowAny
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 
 from books.models import BookModel
 from books.serializers import BookSerializer
 
 class BookViewSet(viewsets.ModelViewSet):
-    queryset = BookModel.objects.all()
+    queryset = BookModel.objects.prefetch_related("authors").all()
     serializer_class = BookSerializer
-    permission_classes = [AllowAny]
+    permission_classes = [IsAuthenticatedOrReadOnly]
